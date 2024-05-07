334f5207f3e3f60340aaceaa02b5fa3cb58b6c8e
Dmitry M <dmitrym@gmail.com>
2024-05-06 20:24:05 -0700

    Remove debugging output from messages.

--- tests/org-noter-org-roam-tests.el
+++ tests/org-noter-org-roam-tests.el
@@ -24,23 +24,16 @@
                                       ;; ADOCUMENT should come after solove-nothing-to-hide
                                       (expect (string-match "solove-nothing-to-hide" (buffer-string)) :to-be-less-than
                                               (string-match "ADOCUMENT" (buffer-string)))
-                                      (message (buffer-string)))))
+                                      )))
 
                               (it "can find an existing heading without creating a new one"
                                   (with-mock-contents
                                    mock-contents-simple-notes-file
                                    (lambda ()
                                       (let* ((found-heading-pos (org-noter--find-create-top-level-heading-for-doc "/tmp/test.pdf" "solove-nothing-to-hide")))
-                                      (message "\n00 ----")
                                       (goto-char found-heading-pos)
                                       (insert "!!")
-                                      (message (buffer-string))
-                                      (message "\n00 ----")
-
-                                      (expect found-heading-pos :to-be 141)
-                                      (message "----")
-                                      (message (buffer-string))
-                                      (message "---- %s" (length (buffer-string)))))))
+                                      (expect found-heading-pos :to-be 141)))))
 
 
                               (it "can create a new heading"
@@ -50,12 +43,7 @@
                                       (expect
                                        ;; org-noter-test-file is defined in test-utils.
                                        (org-noter--find-create-top-level-heading-for-doc "/tmp/some-other-pdf-file.pdf" "SOME HEADING")
-                                       :to-be 162)
-                                      (message "----")
-                                      (message (buffer-string))
-                                      (message "---- %s" (length (buffer-string)))
-
-                                      )))
+                                       :to-be 162))))
                               )
 
 
@@ -69,14 +57,10 @@
                                   (with-mock-contents
                                    mock-contents-simple-notes-file
                                    (lambda ()
-                                      (message "\n11 ----")
                                       (insert "!!")
-                                      (message (buffer-string))
-                                      (message "\n11 ----")
                                       (expect
                                        (org-noter--find-top-level-heading-for-document-path "/tmp/test.pdf")
-                                       :to-be 143)
-                                      (message (buffer-string)))))
+                                       :to-be 143))))
 
 
                               (it "return nil for a non existent top level heading"
@@ -85,8 +69,7 @@
                                    (lambda ()
                                       (expect
                                        (org-noter--find-top-level-heading-for-document-path "/FAKE/PATH/DOESNT/EXIST")
-                                       :to-be nil)
-                                      (message (buffer-string)))))
+                                       :to-be nil))))
                               )
 
 
