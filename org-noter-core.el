49b32bf00f59181031ae8c42b4f46f996187fb4f
Matus Goljer <matus.goljer@gmail.com>
2024-06-02 19:46:42 +0200

    feat(attach): add option to skip confirmation about attachment use

--- org-noter-core.el
+++ org-noter-core.el
@@ -181,9 +181,14 @@ be the other way around."
 (defcustom org-noter-suggest-from-attachments t
   "Suggest document files from attachments (in an Org file).
 When non-nil, org-noter will suggest files from the attachments
-when creating a session, if the document is missing."
+when creating a session, if the document is missing.
+
+If the value 'no-prompt is used, do not ask for confirmation and
+offer an attachment right away."
   :group 'org-noter
-  :type 'boolean)
+  :type '(choice (const :tag "Use attachments but ask user for confirmation" t)
+                 (const :tag "Use attachments without asking for confirmation" no-prompt)
+                 (const :tag "Do not use attachments" nil)))
 
 (defcustom org-noter-separate-notes-from-heading nil
   "When non-nil, add an empty line between each note's heading and content."
@@ -1701,8 +1706,13 @@ DOCUMENT-PATH is a path to a document file."
         (require 'org-attach)
         (let* ((attach-dir (org-attach-dir))
                (attach-list (and attach-dir (org-attach-file-list attach-dir))))
-          (when (and attach-list (y-or-n-p "Do you want to annotate an attached file?"))
-            (setq doc-prop (completing-read "File to annotate: " attach-list nil t))
+          (when (and attach-list
+                     (or (eq org-noter-suggest-from-attachments 'no-prompt)
+                         (y-or-n-p "Do you want to annotate an attached file?")))
+            (setq doc-prop (if (> (length attach-list) 1)
+                               (completing-read "File to annotate: " attach-list nil t
+                                                nil nil (car attach-list))
+                             (car attach-list)))
             (when doc-prop (setq doc-prop (file-relative-name (expand-file-name doc-prop attach-dir)))))))
 
       (unless (org-noter--check-doc-prop doc-prop)
