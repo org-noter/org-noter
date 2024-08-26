786646954c162e67fcf2d711b936b6354a5a9e4c
Dmitry M <dmitrym@gmail.com>
2024-08-25 22:17:01 -0700

    Improve org-roam integration unit tests.

--- tests/org-noter-org-roam-tests.el
+++ tests/org-noter-org-roam-tests.el
@@ -95,11 +95,42 @@
                             (write-region (point-min) (point-max) "/tmp/pubs/notes.org")))
                         (org-noter--create-session-from-document-file-supporting-org-roam nil "/tmp/test.pdf")
                         (expect 'org-noter :to-have-been-called))
+
+
+                    (it "asks for a node interactively when no existing node has been found"
+                        ;; mock the function that would return the node to return nil
+                        (spy-on 'org-noter--get-nodes-with-noter-document-property :and-call-fake (lambda (_) nil))
+                        (spy-on 'org-noter--get-filename-interactively :and-call-fake (lambda () "/tmp/pubs/notes1.org"))
+                        (spy-on 'org-noter)
+                        (with-mock-contents
+                         mock-contents-simple-notes-file
+                         (lambda ()
+                           (write-region (point-min) (point-max) "/tmp/pubs/notes1.org")))
+
+                        (org-noter--create-session-from-document-file-supporting-org-roam nil "/tmp/test.pdf")
+                        (expect 'org-noter--get-filename-interactively :to-have-been-called)
+                        (expect 'org-noter :to-have-been-called))
+
+
+                    (it "does NOT ask for a node interactively when an existing node has been found"
+                        ;; mock the function that would return the node to return nil
+                        (spy-on 'org-noter--find-existing-node-for-document :and-call-fake (lambda (_)  '("/tmp/pubs/notes2.org")))
+                        (spy-on 'org-noter--get-filename-interactively :and-call-fake (lambda () "/tmp/pubs/notes2.org"))
+                        (spy-on 'org-noter)
+                        (with-mock-contents
+                         mock-contents-simple-notes-file
+                         (lambda ()
+                           (write-region (point-min) (point-max) "/tmp/pubs/notes2.org")))
+                        (org-noter--create-session-from-document-file-supporting-org-roam nil "/tmp/test.pdf")
+                        (expect 'org-noter--get-filename-interactively :not :to-have-been-called))
                     )
+
+
 
           (it "sets the hook correctly when org-roam integration is enabled"
               (org-noter-enable-org-roam-integration)
               (expect org-noter-create-session-from-document-hook :to-equal '(org-noter--create-session-from-document-file-supporting-org-roam)))
+
 
 
 )
